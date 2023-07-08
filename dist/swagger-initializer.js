f4b0baab8e6dcccb34455c7825f812e3a5267509
Bata Zoltán <batazo.dad@gmail.com>
2023-07-09 00:09:19 +0200

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
    // the following lines will be replaced by docker/configurator, when it runs in a docker-container
    window.ui = SwaggerUIBundle({
-      url: "../Backend/swagger.yaml",
+      url: "https://bzozoo.github.io/Coordinate-Searcher/Backend/swagger.yaml",
       dom_id: "#swagger-ui",
       deepLinking: true,
       presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
